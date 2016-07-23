9b82485a277ac63384e5f3645ca904d59421db00
John Hanam <jhanam@blackberry.com>
2016-07-22 20:27:11 -0400

    First Commit

---
+++ flights.js
@@ -0,0 +1,58 @@
+var net = require('net');
+var firebase = require("firebase");
+
+firebase.initializeApp({
+  serviceAccount: "./Flights-4a11006477e0.json",
+  databaseURL: "https://flights-e282c.firebaseio.com"
+});
+
+var flightsRef = firebase.database().ref('flights/')
+
+var client = new net.Socket();
+client.connect(30003, '192.168.0.30', function() {
+  console.log('Connected');
+});
+
+client.on('data', function(data) {
+  data = "" + data
+  data = data.replace(/(\r\n|\n|\r)/gm,"");
+
+//        pid = msg[4]
+//       flight = msg[10]
+//       alt = msg[11]
+//       lat = msg[15]
+//       lon = msg[14]
+//       heading = msg[13]
+//       speed = msg[12]
+
+  var d = data.split(",");
+
+  if (d[0] !== "MSG") return;
+
+  console.log("ID: " + d[4] + "  " +
+              "Alt: " + d[11] + "  " +
+              "Lat: " + d[15] + "  " +
+              "Lon: " + d[14] + "  " +
+              "Head: " + d[13] + "  " +
+              "Speed: " + d[12])
+
+  var fb = {}
+
+  if (d[4].length == 0) return;
+  fb["id"] = d[4];
+  
+  if (d[11].length > 0) fb["alt"] = d[11];
+  if (d[15].length > 0) fb["lat"] = d[15];
+  if (d[14].length > 0) fb["lon"] = d[14];
+  if (d[13].length > 0) fb["head"] = d[13];
+  if (d[12].length > 0) fb["speed"] = d[12];
+  
+
+
+  flightsRef.child(d[4]).update(fb);
+
+});
+
+client.on('close', function() {
+  console.log('Connection closed');
+});

5796e3b37d811c7263abe1cb0a4e18032a420ad4
John Hanam <jhanam@blackberry.com>
2016-08-04 23:21:56 -0400

    NM/KM

--- flights.js
+++ flights.js
@@ -1,6 +1,9 @@
 var net = require('net');
 var firebase = require("firebase");
 
+var HOMELAT = 43.388421
+var HOMELONG = -80.448815
+
 firebase.initializeApp({
   serviceAccount: "../Flights-4a11006477e0.json",
   databaseURL: "https://flights-e282c.firebaseio.com"
@@ -48,6 +51,13 @@ client.on('data', function(data) {
   if (d[7].length > 0) fb["time"] = d[7];
   if (ts > 0) fb["timestamp"] = ts;
   fb['updated'] = 1
+
+  if ((d[14].length > 0) && (d[15].length > 0)) {
+    var dist = getDistanceFromLatLonInKm(HOMELAT, HOMELONG, Number(d[14]), Number(d[15]))
+    fb["km"] = dist
+    dist = km2nm(dist)
+    fb["nm"] = dist
+  }
   
   flightsRef.child("inactive/").child(d[4]).remove()
   flightsRef.child("active/").child(d[4]).update(fb);
@@ -73,4 +83,28 @@ setInterval(function () {
 }, 15000)
 
 
+// MATH FUNCTIONS
+// FROM http://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+//  AND http://www.movable-type.co.uk/scripts/latlong.html
 
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+function km2nm(km) {
+  return km * 0.5399568
+}
